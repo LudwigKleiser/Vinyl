2075a6cc7ce48a61bd1dfda7e32fb0462af72abd
Ludiwg Kleiser <ludde_orby@hotmail.com>
2016-10-01 16:29:52 +0200

    Fixade så att ändringarna även görs på git!

---
+++ Vinylsamling1/ListFunctions.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Vinylsamling1
+{
+    class ListFunctions
+    {
+        //Här skapas listorna vi ska använda oss av
+        public static List<string> name = new List<string>();
+        public static List<string> album = new List<string>();
+        public static List<string> artist = new List<string>();
+        public static List<string> year = new List<string>();
+
+
+        // Denna metod körs bara en gång för att kolla om det redan finns en fil, finns det ingen fil så skapar den en ny.
+        // Finns det redan en fil så lägger den in den i listorna
+
+
+        public static void ListApplier()
+        {
+
+            if (File.Exists(@"C:\Users\public\Name.txt") &&
+             File.Exists(@"C:\Users\public\Album.txt") &&
+             File.Exists(@"C:\Users\public\Artist.txt") &&
+             File.Exists(@"C:\Users\public\Year.txt"))
+            {
+                // Här läggs datan från filerna in i listorna.
+                var readFromNames = File.ReadAllLines(@"c:\users\public\Name.txt");
+                foreach (var s in readFromNames)
+                {
+                    name.Add(s);
+
+
+                }
+
+                // https://www.dotnetperls.com/remove-duplicates-list tar bort du
+                List<string> distinct = name.Distinct().ToList();
+                name = distinct;
+
+
+
+
+
+                var readFromAlbum = File.ReadAllLines(@"c:\users\public\Album.txt");
+                foreach (var s in readFromAlbum)
+                {
+                    album.Add(s);
+
+
+                }
+
+                List<string> distinct1 = album.Distinct().ToList();
+                album = distinct1;
+
+                var readFromArtist = File.ReadAllLines(@"c:\users\public\Artist.txt");
+                foreach (var s in readFromArtist)
+                {
+                    artist.Add(s);
+
+
+                }
+                List<string> distinct2 = artist.Distinct().ToList();
+                artist = distinct2;
+
+                var readFromYear = File.ReadAllLines(@"c:\users\public\Year.txt");
+                foreach (var s in readFromYear)
+                {
+                    year.Add(s);
+
+                }
+                List<string> distinct3 = year.Distinct().ToList();
+                year = distinct3;
+
+            }
+
+            else
+            {
+                // Skapar en tom textfil i mappen. Använder man bara create så är filen fortfarande öppen vilket gör att man inte kan gå in i den igen senare i programmet.
+                // Jag använder därför Dispose för att stänga den igen så att vi kommer in i den senare i programmet.
+                File.Create(@"C:\Users\public\Name.txt").Dispose();
+                File.Create(@"C:\Users\public\Album.txt").Dispose();
+                File.Create(@"C:\Users\public\Artist.txt").Dispose();
+                File.Create(@"C:\Users\public\Year.txt").Dispose();
+                Console.WriteLine("Skapar filer.....");
+                Console.WriteLine("Tryck på valfri knapp för att komma till menyn");
+                Console.ReadKey();
+            }
+
+
+        }
+
+
+
+        public static void AddToList() // Metod för att ta in användarens input in i listorna
+        {
+            Console.WriteLine("Skriv in namnet");
+            Console.Write("Namn: ");
+            string tempName = Console.ReadLine();
+            name.Add(tempName);
+
+            Console.WriteLine("Skriv in albumet");
+            Console.Write("Album: ");
+            string tempAlbum = Console.ReadLine();
+            album.Add(tempAlbum);
+
+            Console.WriteLine("Skriv in artisten");
+            Console.Write("Artist: ");
+            string tempArtist = Console.ReadLine();
+            artist.Add(tempArtist);
+
+
+            Console.WriteLine("Skriv in året");
+            Console.Write("år: ");
+            string tempYear = Console.ReadLine();
+            year.Add(tempYear);
+
+        }
+
+        public static void SaveToDisk() // Metod för att spara ner det användaren skrivit in till textfilerna
+        {
+            File.AppendAllLines(@"C:\Users\public\Name.txt", name);
+            File.AppendAllLines(@"C:\Users\public\Album.txt", album);
+            File.AppendAllLines(@"C:\Users\public\Artist.txt", artist);
+            File.AppendAllLines(@"C:\Users\public\Year.txt", year);
+        }
+
+        public static void PrintVinylsToScreen() // Skriver ut vinylerna till användaren.
+        {
+
+
+            for (int i = 0; i < name.Count; i++)
+            {
+                Console.WriteLine("\nNamn: {0}", name[i]);
+                Console.WriteLine("Album: {0}", album[i]);
+                Console.WriteLine("Artist: {0}", artist[i]);
+                Console.WriteLine("År: {0}", year[i]);
+
+            }
+
+
+            Console.Read();
+        }
+
+
+
+    }
+}
+
--- Vinylsamling1/Program.cs
+++ Vinylsamling1/Program.cs
@@ -8,8 +8,60 @@ namespace Vinylsamling1
 {
     class Program
     {
+        static int userValue;
+
         static void Main(string[] args)
         {
+            ListFunctions.ListApplier();
+            bool start = true;
+            while (start)
+            {
+                Console.WriteLine("Hej och välkommen till din vinylsamling");
+                Console.WriteLine("[1] Visa vinyl arkivet");
+                Console.WriteLine("[2] Lägg till i arkivet");
+                Console.WriteLine("[3] ");
+                Console.WriteLine("[4]");
+                string choice = Console.ReadLine();
+                if (choice == "1" || choice == "2" || choice == "3" || choice == "4")
+                {
+                    userValue = int.Parse(choice);
+                }
+                else
+                {
+                    Console.WriteLine("Vänligen ange 1,2,3 eller 4");
+                }
+
+                switch (userValue)
+                {
+                    case 1:
+                        ListFunctions.PrintVinylsToScreen();
+                        Console.Read();
+                        break;
+
+                    case 2:
+                        ListFunctions.AddToList();
+                        ListFunctions.SaveToDisk();
+                        break;
+
+                    case 3:
+                        ListFunctions.PrintVinylsToScreen();
+
+                        break;
+
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+
+
+            }
+
         }
+        
     }
 }
